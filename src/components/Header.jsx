42540da20b8b215d37436de5d954058e505a9b7c
Mostakimw <scientist.expert@gmail.com>
2023-04-11 02:31:01 +0600

    navbar active and default style added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,41 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <div>
       <div className="md:flex justify-between items-center">
         <h1 className="text-3xl font-bold">HireTechies</h1>
-        <div className="flex gap-6">
-          <Link to="/">Home</Link>
+        <div className="flex gap-6 font-semibold">
+          {/* <Link to="/">Home</Link>
           <Link to="/statistics">Statistics</Link>
           <Link to="/applied-jobs">Applied Jobs</Link>
-          <Link to="/blog">Blog</Link>
+          <Link to="/blog">Blog</Link> */}
+          <NavLink
+            exact
+            to="/"
+            className={({ isActive }) => (isActive ? "active" : "default")}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/statistics"
+            className={({ isActive }) => (isActive ? "active" : "default")}
+          >
+            Statistics
+          </NavLink>
+          <NavLink
+            to="/applied-jobs"
+            className={({ isActive }) => (isActive ? "active" : "default")}
+          >
+            Applied Jobs
+          </NavLink>
+          <NavLink
+            to="/blog"
+            className={({ isActive }) => (isActive ? "active" : "default")}
+          >
+            Blog
+          </NavLink>
         </div>
         <Link className="my-btn" to="/all-jobs">
           Start Applying

57b4bb5e10b5765224b8a1496fb89c29823406cd
Mostakimw <scientist.expert@gmail.com>
2023-04-10 08:09:12 +0600

    navbar added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div>
-      <h1>this is nav nav</h1>
+      <div className="flex justify-between items-center">
+        <h1 className="text-3xl font-bold">HireTechies</h1>
+        <div className="flex gap-4">
+          <Link to="/">Home</Link>
+          <Link to="/statistics">Statistics</Link>
+          <Link to="/applied-jobs">Applied Jobs</Link>
+          <Link to="/blog">Blog</Link>
+        </div>
+        <Link className="btn" to="/all-jobs">
+          Start Applying
+        </Link>
+      </div>
     </div>
   );
 };
